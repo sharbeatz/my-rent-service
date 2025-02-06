73b6c8dc2c5729c65ff7aef6c59b3334e9ca2b65
sharbeatz <mr.arthur2003@mail.ru>
2025-02-06 14:09:44 +0300

    Пункт 3 сделан

---
+++ my-rent-service/src/components/App/app.tsx
@@ -0,0 +1,10 @@
+import { JSX } from "react";
+import MainPage from "../../pages/main-page/main-page";
+
+function App(): JSX.Element {
+    return (
+        <MainPage />
+    );
+}
+
+export default App
