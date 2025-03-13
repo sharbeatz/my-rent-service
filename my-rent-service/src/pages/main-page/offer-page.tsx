258e5cf937345443c46060150fd9e40026d758ec
sharbeatz <mr.arthur2003@mail.ru>
2025-03-13 14:21:38 +0300

    Задание 4. Пункт 5 выполнен. Не доделан типы location, isFavorite, City

--- my-rent-service/src/pages/main-page/offer-page.tsx
+++ my-rent-service/src/pages/main-page/offer-page.tsx
@@ -48,9 +48,11 @@ function OfferPage( {offers}: OfferProps) {
           </div>
           <div className="offer__container container">
             <div className="offer__wrapper">
-              <div className="offer__mark">
+              {offers.isPremium ?        
+              (<div className="offer__mark">
                 <span>Premium</span>
-              </div>
+              </div>)
+              : null}
               <div className="offer__name-wrapper">
                 <h1 className="offer__name">
                   {offers.title}
@@ -87,58 +89,39 @@ function OfferPage( {offers}: OfferProps) {
               <div className="offer__inside">
                 <h2 className="offer__inside-title">What&apos;s inside</h2>
                 <ul className="offer__inside-list">
-                  <li className="offer__inside-item">
-                    Wi-Fi
-                  </li>
-                  <li className="offer__inside-item">
-                    Washing machine
-                  </li>
-                  <li className="offer__inside-item">
-                    Towels
-                  </li>
-                  <li className="offer__inside-item">
-                    Heating
-                  </li>
-                  <li className="offer__inside-item">
-                    Coffee machine
-                  </li>
-                  <li className="offer__inside-item">
-                    Baby seat
-                  </li>
-                  <li className="offer__inside-item">
-                    Kitchen
-                  </li>
-                  <li className="offer__inside-item">
-                    Dishwasher
-                  </li>
-                  <li className="offer__inside-item">
-                    Cabel TV
-                  </li>
-                  <li className="offer__inside-item">
-                    Fridge
-                  </li>
+                {offers.goods.map((item) => (
+                                    <li className="offer__inside-item">
+                                    {item}
+                                  </li>
+                ))
+                }
+
+                
                 </ul>
               </div>
               <div className="offer__host">
                 <h2 className="offer__host-title">Meet the host</h2>
                 <div className="offer__host-user user">
                   <div className="offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper">
-                    <img className="offer__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar" />
+                    <img className="offer__avatar user__avatar" src= {offers.host.avatarUrl} width="74" height="74" alt="Host avatar" />
                   </div>
                   <span className="offer__user-name">
-                    Angelina
+                    {offers.host.name}
                   </span>
-                  <span className="offer__user-status">
+                  {offers.host.isPro ?
+                  (<span className="offer__user-status">
                     Pro
-                  </span>
+                  </span>)
+                  : null}
                 </div>
                 <div className="offer__description">
                   <p className="offer__text">
-                    A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
+                    {offers.description}
                   </p>
-                  <p className="offer__text">
+
+                  {/* <p className="offer__text">
                     An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.
-                  </p>
+                  </p> */}
                 </div>
               </div>
               <section className="offer__reviews reviews">
