21eefd6c5531a9886dcc9b29525a373f7d486eef
sharbeatz <mr.arthur2003@mail.ru>
2025-03-28 13:19:09 +0300

    Состояние комментов обновляется. Звездочки не пофикшены. Приступаю к написанию карты

--- my-rent-service/src/components/comment-form/comment-form.tsx
+++ my-rent-service/src/components/comment-form/comment-form.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 
 
 function CommentForm() {
+  const [comment, setComment] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Комментарий отправлен:', comment);
+    <p>{comment}</p>
+    setComment('');
+  }
+
     return(
-        <form className="reviews__form form" action="#" method="post">
+        <form onSubmit={handleSubmit} className="reviews__form form" action="#" method="post">
         <label className="reviews__label form__label" htmlFor="review">Your review</label>
         <div className="reviews__rating-form form__rating">
           <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
@@ -40,12 +54,12 @@ function CommentForm() {
             </svg>
           </label>
         </div>
-        <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
+        <textarea value={comment} onChange={handleChange} className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
         <div className="reviews__button-wrapper">
           <p className="reviews__help">
             To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
           </p>
-          <button className="reviews__submit form__submit button" type="submit" disabled >Submit</button>
+          <button className="reviews__submit form__submit button" type="submit">Submit</button>
         </div>
       </form>
     )
